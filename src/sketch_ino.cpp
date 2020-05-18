36950cfb6eead209887a87275461d7aa6cb4a104
camour <65285695+camour@users.noreply.github.com>
2020-05-18 03:21:50 +0200

    Update sketch_ino.cpp
    Update séance_1

--- src/sketch_ino.cpp
+++ src/sketch_ino.cpp
@@ -8,35 +8,42 @@ void Board::setup(){
 // on fixe les pin en entree et en sorite en fonction des capteurs/actionneurs mis sur la carte
   pinMode(1,INPUT);
   pinMode(0,OUTPUT);
+  pinMode(2,INPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,INPUT);
 }
 
 // la boucle de controle arduino
 void Board::loop(){
   char buf[100];
   int val;
+  int val_boutton;
+  char etat_boutton[4];
   static int cpt=0;
-  static int bascule=0;
+  //static int bascule=0;
   int i=0;
   for(i=0;i<10;i++){
     // lecture sur la pin 1 : capteur de temperature
-    val=analogRead(1);
-    sprintf(buf,"temperature %d",val);
+    val=analogRead(2);
+    val_boutton = digitalRead(4);
+    if (val_boutton == 1){memcpy(etat_boutton,"ON",4);}
+    else {memcpy(etat_boutton,"OFF",4);}
+    sprintf(buf,"luminosite %d, bouton %s",val, etat_boutton);
     Serial.println(buf);
+
     if(cpt%5==0){
-        // tous les 5 fois on affiche sur l ecran la temperature
-      sprintf(buf,"%d",val);
+        // tous les 5 fois on affiche sur l ecran la luminosite
+      sprintf(buf,"%d, %d",val,val_boutton);
       bus.write(1,buf,100);
     }
     cpt++;
     sleep(1);
   }
 // on eteint et on allume la LED
-  if(bascule)
-    digitalWrite(0,HIGH);
+  if(val_boutton)
+    digitalWrite(3,HIGH);
   else
-    digitalWrite(0,LOW);
-  bascule=1-bascule;
-  
+    digitalWrite(3,LOW);
+  //bascule=1-bascule;
+
 }
-
-

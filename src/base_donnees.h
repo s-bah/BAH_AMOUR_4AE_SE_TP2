e1cbf3106dcd3976ab183819de6ca5288dd97aac
camour <65285695+camour@users.noreply.github.com>
2020-05-26 12:49:50 +0200

    Add files via upload

---
+++ src/base_donnees.h
@@ -0,0 +1,13 @@
+#ifndef BASE_DONNEES_H_INCLUDED
+#define BASE_DONNEES_H_INCLUDED
+
+#include <fstream>
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+
+bool rechercher_base_donnees(char* tag_recherche);
+
+
+
+#endif // BASE_DONNEES_H_INCLUDED
